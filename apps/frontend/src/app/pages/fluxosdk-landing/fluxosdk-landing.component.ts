c87308c4599bdab75eb33926382d6e0d5f186cb7
luiizr <luizroberto312005@gmail.com>
2026-05-07 12:57:33 -0300

    salvando landing-page

---
+++ apps/frontend/src/app/pages/fluxosdk-landing/fluxosdk-landing.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fluxosdk-landing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './fluxosdk-landing.component.html',
+  styleUrls: ['./fluxosdk-landing.component.scss'],
+})
+export class FluxosdkLandingComponent {}
